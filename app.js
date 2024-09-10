327f10e27e6818d66c83755a480269990771eea7
Rohankhan999 <rohankhan08453@gmail.com>
2024-09-10 23:51:04 +0500

    homework

---
+++ app.js
@@ -0,0 +1,56 @@
+// chapters 12-13//(IF...ELSE & ELSE IF STATEMENT,TESTING SET OF CONDITIONS);
+
+//Question 01//
+
+// var num;
+// var character = prompt("Enter a Character!")
+// if(character  === "A"){
+//     num = 65
+// }
+// else if (character === "a"){
+//     num = 97
+// }
+// else if (character === "Z"){
+//     num = 90
+// }
+// else if(character === "z"){
+//  num = 122
+// }
+// alert(character + " = " + num);
+
+//Question 02//
+
+// var n1 = prompt("Enter Your First Number!");
+// var n2 =  prompt("Enter Your Second Number!");
+
+// if(n1 > n2){
+//     n1 = "Your first number " + n1 + " is greater than " + n2;
+// }
+// else if (n1 < n2){
+//     n1 = "Your first number " + n1 + " is less than " + n2;
+// }
+// else if (n1 === n2){
+//     n1 = "your first number " + n1 + " is Equal to " + n2 ;
+// }
+// alert(n1);
+
+//Question 03//
+
+// var number = +prompt("Enter Your Number!");
+// if (number < 0) {
+//     number = "your number " + number + " is negative Number";
+// }
+// else if (number > 0) {
+//     number = "your number " + number + " is Positve Number";
+// }
+// else if (number === 0){
+//     number = "your number " + number + " is Zero Number";
+// }
+// alert(number);
+
+//Question 04//
+
+var vowels =prompt("Enter a Alphabet");
+if(vowels .toLowerCase === "a"){
+    Alert
+}
